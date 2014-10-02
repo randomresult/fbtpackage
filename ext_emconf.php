80bd2c805af184c0889fb45a7151279e0e0209f9
Harry Glatz <glatz@analog.de>
2014-10-02 10:40:35 +0200

    [TASK] Initial Commit

---
+++ ext_emconf.php
@@ -0,0 +1,56 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "fbtpackage".
+ *
+ * Auto generated 01-10-2014 12:24
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'FluidBootstrapTheme Distribution',
+	'description' => 'Demo Install with pages and Content',
+    'category' => 'distribution',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => 'cms,extbase,fluid,flux,vhs,fluidbootstraptheme,fluidcontent,fluidpages,indexed_search',
+	'conflicts' => ' ',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Harry Glatz',
+	'author_email' => 'glatz@analog.de',
+	'author_company' => 'analog multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.5-6.2.99',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+			'flux' => '7.1.0 ',
+			'vhs' => '2.0.2',
+            'fluidpages' => '3.1.1 ',
+            'fluidcontent' => ' 	4.1.0 ',
+            'fluidbootstraptheme' => '1.1.0',
+
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'suggests' => array(
+	),
+);
